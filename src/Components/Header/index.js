d50bc579c85f67ffa1eb3a5521c865f29e6763d4
Mykaela Saenz <mickycore@gmail.com>
2022-12-05 11:21:55 -0600

    updating header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -7,7 +7,7 @@ function Header(props) {
     return (
         <div>
             <header id="header1">
-                <a href="/" id="userName">
+                <a href="https://mykaelas.github.io/Mykaela-Portfolio/" id="userName">
                     <h1>
                         Mykaela Saenz
                     </h1>
