87baf3ee5435700904590dc317c1981d09dd08ae
Mykaela Saenz <mickycore@gmail.com>
2023-01-27 14:56:34 -0600

    added skills to my about section

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -7,9 +7,12 @@ function About() {
         <img src={Profile} alt="Mykaela" className="mykaelaPicture" />
         <p className="my-2">
           Hello, my name is Mykaela Saenz, I am a JavaScript Developer with a Full Stack certificate from Rice University.
-          I have worked on various projects that have included multiple different JavaScript libraries and extensions.
+          I have worked on various projects that have included multiple different languages and libraries such as JavaScript,
+          HTML, CSS, ReactJs, and NodeJs just to name a few. 
+          <br />
           Not only have I worked on solo projects but I've also been fortunate enough to work in groups with other talented
           classmates of mine. To view my work please click on my Portfolio tab. 
+        
         </p>
       </div>
   );
